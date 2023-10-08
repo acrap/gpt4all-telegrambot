c4caaa57386a810661c3f370d805e8f9c3087eb1
Andrey Strunin <acrap@mail.ru>
2023-10-08 22:54:29 +0200

    Started to implement roles

--- model_thread.py
+++ model_thread.py
@@ -15,15 +15,24 @@ class ModelThread():
 
     def initModel(self, model_name, role=None): 
         print(self)
-        thread = Thread(target=worker, args=(model_name, self.in_queue, self.out_queue,))
+        thread = Thread(target=worker, args=(model_name, role, self.in_queue, self.out_queue,))
         thread.start()
         self.isRunning = True
 
 # Object that signals shutdown
 _sentinel = object()
 
-def worker(model_name, in_q, out_q):
+def return_default_role():
+    role = dict()
+    role = {'system_prompt':'You an helpful AI assistent and you behave like an AI research assistant. You use a tone that is technical and scientific. Below is our chat history, please continue the conversaion.',
+                    'prompt_template':'### Instruction:\n{0}\n### Response:\n'}
+    return role
+    
+def worker(model_name, role, in_q, out_q):
     model = GPT4All(model_name)
-    for data in iter(in_q.get, _sentinel):
-        output = model.generate(data)
-        out_q.put(output)
+    if role is None:
+        role = return_default_role()
+    with model.chat_session(role['system_prompt'], role['prompt_template']):
+        for data in iter(in_q.get, _sentinel):
+            output = model.generate(data)
+            out_q.put(output)
