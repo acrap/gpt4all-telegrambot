4004db5d44f6b09afb04c29c7dbd44177ee7757c
Andrey Strunin <acrap@mail.ru>
2023-10-08 23:18:22 +0200

    Improved role support

--- gpt-bot.py
+++ gpt-bot.py
@@ -3,23 +3,54 @@ from telegram.ext import ApplicationBuilder, CommandHandler,MessageHandler, Cont
 import os
 from queue import Queue
 from model_thread import ModelThread
+import yaml
 
 model_thread = ModelThread()
 prompt_queue=Queue()
 response_queue=Queue()
+roles_dir = "./roles"
+role = "default"
+
+def list_roles():
+    res = []
+    for file_path in os.listdir(roles_dir):
+        # check if current file_path is a file
+        if os.path.isfile(os.path.join(roles_dir, file_path)):
+            # add filename to list
+            res.append(file_path.replace('.role',''))
+    return res
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     """Send a message when the command /start is issued."""
     user = update.effective_user
     await update.message.reply_text("Hi, {user}!Please enter the password using /pswd command:")
-    
+
+async def role(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if update.message.text == "/role":
+        #just show the roles available
+        roles = list_roles()
+        # using list comprehension
+        rolesStr = ' '.join([str(elem) for elem in roles])
+        await update.message.reply_text(rf"Here are the available roles(switch to it with `/role rolename`):{rolesStr}")
+    else:
+        role_name = update.message.text.replace('/role ','')
+        if os.path.isfile(rf"{roles_dir}/{role_name}.role"):
+            global role
+            role = role_name
+
+def load_role(role_name):
+    with open(rf"{roles_dir}/{role_name}.role","r") as file_object:
+        data=yaml.load(file_object,Loader=yaml.SafeLoader)
+    return data['role']
+
 def is_usr_verified(update: Update):
     user = update.effective_user
     if user.id == int(os.environ['USER_ID']):
         print("User verified")
         model_name = os.environ['GPT_MODEL']
-        model_thread.initModel(model_name,None)
+        _role = load_role(role)
+        model_thread.initModel(model_name,_role)
         return True
     else:
         print("User is not verified")
@@ -39,6 +70,9 @@ app = ApplicationBuilder().token(os.environ['TGRAM_TOKEN']).build()
 
 # on different commands - answer in Telegram
 app.add_handler(CommandHandler("start", start))
+
+# on different commands - answer in Telegram
+app.add_handler(CommandHandler("role", role))
 
 # on non command i.e message - echo the message on Telegram
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, chat))
