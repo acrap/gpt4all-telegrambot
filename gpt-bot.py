9df14542617ddba329124dcde47d4ca40f55f8b6
Andrey Strunin <acrap@mail.ru>
2023-10-08 21:27:19 +0200

    Verify user id, remove pswd logic

--- gpt-bot.py
+++ gpt-bot.py
@@ -16,37 +16,32 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = update.effective_user
     await update.message.reply_text("Hi, {user}!Please enter the password using /pswd command:")
     
-
-async def pswd(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+def is_usr_verified(update: Update):
     user = update.effective_user
-    print(user)
-    right_pswd=os.environ['GPT_BOT_PSW']
-    typed_pswd = update.message.text.replace("/pswd ","")
-    print(rf"{typed_pswd} {right_pswd}")
-    if right_pswd==typed_pswd:
-        await update.message.reply_text("Password is ok")
+    if user.id == int(os.environ['USER_ID']):
+        print("User verified")
         global model
         model_name = os.environ['GPT_MODEL']
         model = GPT4All(model_name)
+        return True
     else:
-        await update.message.reply_text("Password is not valid")
+        print("User is not verified")
+    return False
     
 async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if model is None:
+        is_usr_verified(update)
     if model is not None:
         output = model.generate(update.message.text)
         await update.message.reply_text(output)
-    else:
-        await update.message.reply_text("Use /pswd command to login")
-    
 
 app = ApplicationBuilder().token(os.environ['TGRAM_TOKEN']).build()
 
 
 # on different commands - answer in Telegram
 app.add_handler(CommandHandler("start", start))
-app.add_handler(CommandHandler("pswd", pswd))
-# on non command i.e message - echo the message on Telegram
 
+# on non command i.e message - echo the message on Telegram
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, chat))
 
 app.run_polling()
